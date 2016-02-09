6e418340e92f4ef3eac1c405fb6fb8781b934232
agostino_aless@yahoo.it <agostino_aless@yahoo.it>
2016-02-09 18:21:56 +0100

    Bug fix for Input Form

--- GUI/InputForm.cs
+++ GUI/InputForm.cs
@@ -28,28 +28,37 @@ namespace ProjectONE.GUI
         //insert attribute and control consinstency of data
         private void button1_Click(object sender, EventArgs e)
         {
-            bool problem = false;
-                if (!cmb_tipo_attributo.SelectedItem.ToString().Equals("String"))
+            if(txt_nome_attributo.Text.Equals(""))
+            {
+                MessageBox.Show("You must enter a name for this attribute", "Input Form");
+                return;
+            }
+
+                if (!cmb_tipo_attributo.SelectedItem.ToString().Equals("String")) //attribute of type Integer
                 {
                     int temp1, temp2;
 
+                    if(txt_range1.Text.Equals("") || txt_range2.Text.Equals(""))
+                    {
+                        MessageBox.Show("You must enter upper and lowerbound", "Input Form");
+                        return;
+                    }
+
                     if (int.TryParse(txt_range1.Text, out temp1) && int.TryParse(txt_range2.Text, out temp2)) //both the values of the range are inserted?
-                    { 
+                    {
                         if (temp1 <= temp2) //range: n1 <= n2?
-                                               
-                            this.CreateTreeForm.passParams(this.type, txt_nome_attributo.Text, cmb_tipo_attributo.SelectedItem.ToString(), ((int)temp1).ToString(), ((int)temp2).ToString()); 
+
+                            this.CreateTreeForm.passParams(this.type, txt_nome_attributo.Text, cmb_tipo_attributo.SelectedItem.ToString(), ((int)temp1).ToString(), ((int)temp2).ToString());
                         else
-                            problem = true;
+                        {
+                            MessageBox.Show("You must enter correctly lower and upperbound", "Input Form");
+                            return;
+                        }
                     }
                 }
-            else { 
-                problem = true;
-                    if(problem)
-                        MessageBox.Show("Please insert appropriate attributes");
-                    return;
-                }
-            //attribute is of type string
-            this.CreateTreeForm.passParams(this.type, txt_nome_attributo.Text, cmb_tipo_attributo.SelectedItem.ToString(), "", "");
+            else { //attribute of type String
+                this.CreateTreeForm.passParams(this.type, txt_nome_attributo.Text, cmb_tipo_attributo.SelectedItem.ToString(), "", "");
+            }
         }
 
         //control values in combobox. Id est, you cannot write "Double", "integer", "hello world" and so on. Only "String" or "Integer"
